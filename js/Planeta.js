408a08277913d97e3a2cfb03b29840d67c5e650f
franciscosaiote <57148701+franciscosaiote@users.noreply.github.com>
2022-06-01 01:49:34 +0100

    ponto 2
    - implementação movimento nave (falta uma parte que não está a funcionar devidamente)
    - tentativa de implementação da câmera (não está a funcionar)

--- js/Planeta.js
+++ js/Planeta.js
@@ -5,9 +5,9 @@ var scene, renderer;
 
 var geometry, material, mesh;
 
-var prism ;
-var prism1;
-var prism2;
+var planet;
+var shipAux;
+var shipBody;
 
 var limit = 50;
 
@@ -28,17 +28,7 @@ var activeCamera = 0;
 
 function createPlanet(x, y, z){
 	
-	var planet = new THREE.Object3D();
 	
-	
-	geometry = new THREE.SphereGeometry( 12, 32, 16 );
-	material = new THREE.MeshBasicMaterial( { color: 0x000080, wireframe: false } );
-	mesh = new THREE.Mesh(geometry, material);
-	
-	planet.add(mesh);
-	planet.position.set(x, y, z);
-	
-	scene.add(planet);
 }
 
 
@@ -47,10 +37,33 @@ function createScene(){
 	scene = new THREE.Scene();
 	
 	scene.add(new THREE.AxisHelper(10));
-	
-	
-	createPlanet(0, 0, 0);
 
+    planet = new THREE.Object3D();
+	
+	
+	geometry = new THREE.SphereGeometry( 12, 32, 16 );
+	material = new THREE.MeshBasicMaterial( { color: 0x000080, wireframe: true } );
+	mesh = new THREE.Mesh(geometry, material);
+	
+	planet.add(mesh);
+	planet.position.set(0,0,0);
+	
+	scene.add(planet);
+	
+	shipBody = new THREE.Object3D();
+	
+	
+	geometry = new THREE.CylinderGeometry( 3, 3, 5,64);
+	material = new THREE.MeshBasicMaterial( { color: 0x0600560, wireframe: false } );
+	mesh = new THREE.Mesh(geometry, material);
+	
+	shipBody.add(mesh);
+	shipAux = new THREE.Object3D();
+
+    shipAux.add(shipBody);
+    scene.add(shipAux);
+    //shipBody.position.y = 14.4;
+    shipBody.position.x = 14.4;
 }
 
 function createFrontalCamera() {
@@ -63,16 +76,70 @@ function createFrontalCamera() {
 
 }
 
+function createShipCamera() {
+
+    camera[1] = new THREE.OrthographicCamera(-50, 50, 25, -25, 0.1, 10000);
+    var objectPosition = new THREE.Vector3();
+    shipBody.getWorldPosition(objectPosition);
+    camera[1].lookAt(objectPosition);
+    camera[1].position.y = objectPosition.y+5.0;
+    camera[1].position.z = objectPosition.z-5.0;
+    camera[1].position.x = objectPosition.x;
+
+}
+
+function onDocumentKeyDown(event){
+    var keyCode = event.keyCode;
+    keyMap[keyCode] = true;
+
+    if(keyMap[52]) { //4
+      material.wireframe = !material.wireframe;
+    }
+}
+
+function onDocumentKeyUp(event){
+    var keyCode = event.keyCode;
+    keyMap[keyCode] = false;
+}
+
 
 
 function update(){
 	
+    if (keyMap[37]) {//left
+        shipAux.rotateY(0.05);
+    }
+
+    if (keyMap[38]) {//up
+        shipAux.rotateX(0.05);
+
+    }
+    if (keyMap[39]) {//right
+        shipAux.rotateY(-0.05);
+
+    }
+    if (keyMap[40]) {//down
+        shipAux.rotateX(-0.05);
+    }
+    
+    if(keyMap[49]) { //1
+        activeCamera = 0;
+    }
+    if(keyMap[50]) { //2
+        activeCamera = 1;
+    }
+    if(keyMap[51]) { //3
+        activeCamera = 2;
+    }
+
+
+
 }
 
 function createCameras(){
 	
 	createFrontalCamera();
-    //createTopCamera();
+    createShipCamera();
     //createLateralCamera();
 
 }
@@ -88,17 +155,24 @@ function init() {
     createScene();
     createCameras();
 
-    //document.addEventListener("keydown", onDocumentKeyDown, true);
-    //document.addEventListener("keyup", onDocumentKeyUp, true);
+    document.addEventListener("keydown", onDocumentKeyDown, true);
+    document.addEventListener("keyup", onDocumentKeyUp, true);
 
 }
 
 function animate() {
+
 
     update();
 
     renderer.render(scene, camera[activeCamera]);
 
     requestAnimationFrame( animate );
+
+    
+    //planet.rotateY(0.004);
+
+    //shipAux.rotateY(-0.05);
+    /*shipAux.rotateX(0.0005); */
 
 }
