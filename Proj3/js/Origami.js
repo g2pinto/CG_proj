dee9adcce59f3d8cc021444378789dda52c7d02c
franciscosaiote <57148701+franciscosaiote@users.noreply.github.com>
2022-06-15 00:54:25 +0100

    rotation

--- Proj3/js/Origami.js
+++ Proj3/js/Origami.js
@@ -21,11 +21,14 @@ var activeCamera = 0;
 
 
 var delta;
-var origami1;
-var origami2;
+var origami10;
+var origami11;
+
+var origami20;
+var origami21;
 
 function createFirstOrigami(){
-    origami1 = new THREE.Object3D();
+    origami10 = new THREE.Object3D();
     const geometry = new THREE.BufferGeometry();
     // create a simple square shape. We duplicate the top left and bottom right
     // vertices because each vertex needs to appear once per triangle.
@@ -41,22 +44,22 @@ function createFirstOrigami(){
     const mesh = new THREE.Mesh( geometry, material );
     //geom.faces.push(new THREE.Face3(0, 1, 2, normal));
 
-    origami1.add(mesh);
+    origami10.add(mesh);
     //scene.add(mesh);
 
-    origami2 = origami1.clone()
-    origami2.rotateY(Math.PI)
-    origami1.add(origami2)
+    origami11 = origami10.clone()
+    origami11.rotateY(Math.PI)
+    origami10.add(origami11)
 
-    origami1.position.set(-40, 0, 0);
+    origami10.position.set(-40, 0, 0);
 
-    scene.add(origami1);
+    scene.add(origami10);
 
 
 }
 
 function createSecondOrigami(){
-    origami1 = new THREE.Object3D();
+    origami20 = new THREE.Object3D();
     const geometry = new THREE.BufferGeometry();
     // create a simple square shape. We duplicate the top left and bottom right
     // vertices because each vertex needs to appear once per triangle.
@@ -70,7 +73,7 @@ function createSecondOrigami(){
     geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
     const material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
     const mesh = new THREE.Mesh( geometry, material );
-    origami1.add(mesh);
+    origami20.add(mesh);
 
     const geometry1 = new THREE.BufferGeometry();
     // create a simple square shape. We duplicate the top left and bottom right
@@ -85,7 +88,7 @@ function createSecondOrigami(){
     geometry1.setAttribute( 'position', new THREE.BufferAttribute( vertices1, 3 ) );
     const material1 = new THREE.MeshBasicMaterial( { color: 0xfff000, wireframe: true } );
     const mesh1 = new THREE.Mesh( geometry1, material1 );
-    origami1.add(mesh1);
+    origami20.add(mesh1);
 
     const geometry2 = new THREE.BufferGeometry();
     // create a simple square shape. We duplicate the top left and bottom right
@@ -100,7 +103,7 @@ function createSecondOrigami(){
     geometry2.setAttribute( 'position', new THREE.BufferAttribute( vertices2, 3 ) );
     const material2 = new THREE.MeshBasicMaterial( { color: 0x5fff00, wireframe: true } );
     const mesh2 = new THREE.Mesh( geometry2, material2 );
-    origami1.add(mesh2);
+    origami20.add(mesh2);
 
 
     const geometry3 = new THREE.BufferGeometry();
@@ -111,13 +114,13 @@ function createSecondOrigami(){
     ] );
 
     geometry3.setAttribute( 'position', new THREE.BufferAttribute( vertices3, 3 ) );
-    const material3 = new THREE.MeshBasicMaterial( { color: 0x5fff00, wireframe: true } );
+    const material3 = new THREE.MeshBasicMaterial( { color: 0x3333ff, wireframe: true } );
     const mesh3 = new THREE.Mesh( geometry3, material3 );
-    origami1.add(mesh3);
-    origami2 = origami1.clone()
-    origami2.rotateY(Math.PI)
-    origami1.add(origami2)
-    scene.add(origami1);
+    origami20.add(mesh3);
+    origami21 = origami20.clone()
+    origami21.rotateY(Math.PI)
+    origami20.add(origami21)
+    scene.add(origami20);
 
 }
 
@@ -167,6 +170,15 @@ function onResize(){
     //resizePerspectiveCamera(2);
 }
 
+function onDocumentKeyDown(event){
+    var keyCode = event.keyCode;
+    keyMap[keyCode] = true;
+}
+
+function onDocumentKeyUp(event){
+    var keyCode = event.keyCode;
+    keyMap[keyCode] = false;
+}
 
 function update(){
     
@@ -175,19 +187,19 @@ function update(){
     
     
 	
-    if (keyMap[37]) {//left
-
+    if (keyMap[69]) { // second origami left E(e)
+        origami20.rotateY(-Math.PI/180)
     }
 
-    if (keyMap[38]) {//up
-
-
-    }
-    if (keyMap[39]) {//right
+    if (keyMap[82]) {//second origami right R(r)
+        origami20.rotateY(Math.PI/180)
 
     }
-    if (keyMap[40]) {//down
-
+    if (keyMap[81]) {//first origami left Q(q)
+        origami10.rotateY(-Math.PI/180)
+    }
+    if (keyMap[87]) {//first origami right W(w)
+        origami10.rotateY(Math.PI/180)
     }
     
     if(keyMap[49]) { //1
